e36654c54fd37ab77adbad9c4c397a95c4dd9fe8
Niko Kostič <kostic.niko@icloud.com>
2022-03-31 21:54:23 +0200

    še dodatni izračuni

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -14,4 +14,7 @@ function main (numOne, numTwo){
 }
 
 console.log(main(15, 3));  // change this line
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
 module.exports = main;
